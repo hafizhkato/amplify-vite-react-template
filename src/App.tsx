9872b9555efea26579ec3573501f768117b69020
Hafizh Zulkifli <hafizhzulkifli@gmail.com>
2025-02-21 10:25:28 +0800

    delete everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+
 import FAQ from "./scenes/faq";
 
-const client = generateClient<Schema>();
+
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
 
   return (
     <main>
